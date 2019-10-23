5f99ea5db8af997c2e6886b468340142bc8e59aa
Daniel Siekawitch <daniel.siekawitch@gmail.com>
2019-10-23 20:28:54 +0000

    Done.

---
+++ app/models/super_hero.rb
@@ -0,0 +1,19 @@
+class SuperHero
+
+  attr_reader :name, :power, :bio
+
+  SUPERHEROES = []
+
+  def initialize(params)
+    @name = params[:name]
+    @power = params[:power]
+    @bio = params[:bio]
+
+    SUPERHEROES << self
+  end
+
+  def self.all
+    # renders all super_heros or empties the array instead of using a clear method.
+    SUPERHEROES
+  end
+end
