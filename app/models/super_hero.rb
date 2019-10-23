a815a9caf983dd3f8e8315005a5ae26a71bcf676
Daniel Siekawitch <daniel.siekawitch@gmail.com>
2019-10-23 20:22:27 +0000

    Automatically backed up by Learn

--- app/models/super_hero.rb
+++ app/models/super_hero.rb
@@ -1,4 +1,5 @@
 class SuperHero
+
   attr_reader :name, :power, :bio
 
   SUPERHEROES = []
@@ -7,10 +8,12 @@ class SuperHero
     @name = params[:name]
     @power = params[:power]
     @bio = params[:bio]
+
     SUPERHEROES << self
   end
 
   def self.all
+    # renders all super_heros or empties the array instead of using a clear method.
     SUPERHEROES
   end
 end
